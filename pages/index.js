0cf565b3a234c5563c64d39ed7f5888701c8e5a6
Dylan <dylanb@zillow.com>
2020-12-19 17:27:54 -0800

    update GA tag

--- pages/index.js
+++ pages/index.js
@@ -84,10 +84,17 @@ export default function Home() {
         <title>Dylan Bussone</title>
         <link rel="icon" href="/favicon.ico" />
         {/* Global site tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-54162808-1"
-        />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-54162808-1" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-54162808-1');
+                `,
+            }}
+          />
       </Head>
 
       <Header inView={inView} />
